b85b0d90b539d6fe543cc4b857fa03c9cb428f1a
cyruscvc <cyruscvc@gmail.com>
2026-06-25 12:59:27 +0400

    Update Mltext.java

--- src/android/Mltext.java
+++ src/android/Mltext.java
@@ -231,4 +231,191 @@ public class Mltext extends CordovaPlugin {
                                                             linecorners.put("x1", "");
                                                             linecorners.put("y1", "");
 
-                                                            line
+                                                            linecorners.put("x2", "");
+                                                            linecorners.put("y2", "");
+
+                                                            linecorners.put("x3", "");
+                                                            linecorners.put("y3", "");
+
+                                                            linecorners.put("x4", "");
+                                                            linecorners.put("y4", "");
+                                                        }
+                                                        else {
+                                                            linecorners.put("x1", line.getCornerPoints()[0].x);
+                                                            linecorners.put("y1", line.getCornerPoints()[0].y);
+
+                                                            linecorners.put("x2", line.getCornerPoints()[1].x);
+                                                            linecorners.put("y2", line.getCornerPoints()[1].y);
+
+                                                            linecorners.put("x3", line.getCornerPoints()[2].x);
+                                                            linecorners.put("y3", line.getCornerPoints()[2].y);
+
+                                                            linecorners.put("x4", line.getCornerPoints()[3].x);
+                                                            linecorners.put("y4", line.getCornerPoints()[3].y);
+                                                        }
+
+                                                        linepoints.put(linecorners);
+
+                                                        JSONObject lineframeobj = new JSONObject();
+
+                                                        if (line.getBoundingBox()==null)
+                                                        {
+                                                            lineframeobj.put("x", "");
+                                                            lineframeobj.put("y", "");
+                                                            lineframeobj.put("height","");
+                                                            lineframeobj.put("width", "");
+                                                        }
+                                                        else
+                                                        {
+                                                            lineframeobj.put("x", line.getBoundingBox().left);
+                                                            lineframeobj.put("y", line.getBoundingBox().bottom);
+                                                            lineframeobj.put("height", line.getBoundingBox().height());
+                                                            lineframeobj.put("width", line.getBoundingBox().width());
+                                                        }
+
+
+                                                        lineframe.put(lineframeobj);
+
+                                                        for (Text.Element element : line.getElements())
+                                                        {
+
+                                                            wordtext.put(element.getText());
+
+                                                            JSONObject wordcorners = new JSONObject();
+
+                                                            if (element.getCornerPoints()==null)
+                                                            {
+                                                                wordcorners.put("x1", "");
+                                                                wordcorners.put("y1", "");
+
+                                                                wordcorners.put("x2", "");
+                                                                wordcorners.put("y2", "");
+
+                                                                wordcorners.put("x3", "");
+                                                                wordcorners.put("y3", "");
+
+                                                                wordcorners.put("x4", "");
+                                                                wordcorners.put("y4", "");
+                                                            }
+                                                            else
+                                                            {
+                                                                wordcorners.put("x1", element.getCornerPoints()[0].x);
+                                                                wordcorners.put("y1", element.getCornerPoints()[0].y);
+
+                                                                wordcorners.put("x2", element.getCornerPoints()[1].x);
+                                                                wordcorners.put("y2", element.getCornerPoints()[1].y);
+
+                                                                wordcorners.put("x3", element.getCornerPoints()[2].x);
+                                                                wordcorners.put("y3", element.getCornerPoints()[2].y);
+
+                                                                wordcorners.put("x4", element.getCornerPoints()[3].x);
+                                                                wordcorners.put("y4", element.getCornerPoints()[3].y);
+                                                            }
+
+                                                            wordpoints.put(wordcorners);
+
+                                                            JSONObject wordframeobj = new JSONObject();
+                                                            if (element.getBoundingBox()==null)
+                                                            {
+                                                                wordframeobj.put("x", "");
+                                                                wordframeobj.put("y", "");
+                                                                wordframeobj.put("height","");
+                                                                wordframeobj.put("width", "");
+                                                            }
+                                                            else
+                                                            {
+                                                                wordframeobj.put("x", element.getBoundingBox().left);
+                                                                wordframeobj.put("y", element.getBoundingBox().bottom);
+                                                                wordframeobj.put("height", element.getBoundingBox().height());
+                                                                wordframeobj.put("width", element.getBoundingBox().width());
+                                                            }
+
+
+
+                                                            wordframe.put(wordframeobj);
+                                                            //wordframe.put(element.getBoundingBox());
+
+                                                        }
+                                                    }
+                                                }
+
+
+                                                blockobj.put("blocktext", blocktext);
+                                                blockobj.put("blockpoints", blockpoints);
+                                                blockobj.put("blockframe", blockframe);
+
+                                                lineobj.put("linetext", linetext);
+                                                lineobj.put("linepoints", linepoints);
+                                                lineobj.put("lineframe", lineframe);
+
+                                                wordobj.put("wordtext", wordtext);
+                                                wordobj.put("wordpoints", wordpoints);
+                                                wordobj.put("wordframe", wordframe);
+
+                                                resultobj.put("foundText", true);
+                                                resultobj.put("blocks", blockobj);
+                                                resultobj.put("lines", lineobj);
+                                                resultobj.put("words", wordobj);
+
+                                                callbackContext.success(resultobj);
+                                                PluginResult r = new PluginResult(PluginResult.Status.OK);
+                                                callbackContext.sendPluginResult(r);
+                                            }
+                                        }
+                                        catch (JSONException e)
+                                        {
+                                            callbackContext.error(String.valueOf(e));
+                                            PluginResult r = new PluginResult(PluginResult.Status.ERROR);
+                                            callbackContext.sendPluginResult(r);
+                                        }
+                                    })
+                                    .addOnFailureListener(
+                                            new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    callbackContext.error("Error with Text Recognition Module");
+                                                    PluginResult r = new PluginResult(PluginResult.Status.ERROR);
+                                                    callbackContext.sendPluginResult(r);
+                                                }
+                                            });
+
+                        }
+                        else
+                        {
+                            callbackContext.error("Error in uri or base64 data!");
+                            PluginResult r = new PluginResult(PluginResult.Status.ERROR);
+                            callbackContext.sendPluginResult(r);
+                        }
+                    } catch (Exception e) {
+                        callbackContext.error("Main loop Exception");
+                        PluginResult r = new PluginResult(PluginResult.Status.ERROR);
+                        callbackContext.sendPluginResult(r);
+                    }
+                }
+            });
+
+            return true;
+
+        }
+            return false;
+    }
+
+
+    private Bitmap decodeBitmapUri(Context ctx, Uri uri) throws FileNotFoundException
+    {
+        int targetW = 600;
+        int targetH = 600;
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(ctx.getContentResolver().openInputStream(uri), null, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        return BitmapFactory.decodeStream(ctx.getContentResolver()
+                .openInputStream(uri), null, bmOptions);
+    }
+}
